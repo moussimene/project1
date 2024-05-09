19ac615b448fc5a0870468ca2a76a9bcbb7b5b27
moussimene <mlle.moussaoui.imene@gmail.com>
2024-05-09 11:01:04 +0100

    version finale du project1

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-quotes={"quote1","quote2","quote3","quote4"}
+
+let quotes=["To condense fact from the vapor of nuance","The brightest light casts the darkest shadow",
+"It has no eyes. Zack, why doesn’t it have any eyes?",
+"Beef had hit $300 a kilo. Not that he could recall the last time he’d tasted real beef"]
+
+
+let generateBtn=document.querySelector(".generate-button")//document.getElementsByClassName("generate-button")
+
+
+function generateQuote(){
+    var quoteGeneree = quotes[Math.floor(Math.random() * quotes.length)];
+    const quote=document.querySelector(".quote") 
+    quote.innerHTML=`<p> ${quoteGeneree}</p>`
+
+    
+}
+
+generateBtn.addEventListener("click", generateQuote);
